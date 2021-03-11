8456d426080840506cb515a6c8523229595518be
jarrod10 <jarrodadair43@hotmail.com>
2021-03-11 20:10:58 +1100

    basic client framework

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,62 @@
-public class Client {
+import java.io.*;
+import java.net.*;
 
-    public static void main(String[] args) {
+class client {
 
+    public static void main(String[] args) throws Exception {
+        boolean verbose = false;
+
+        System.out.println("Client's Alive!");
+        if (args.length > 0) {
+            if (args.length > 2) {
+                System.out.println("Too many args!");
+                System.out.println("Closing");
+                System.exit(0);
+            }
+            
+            for (String arg : args) {
+                if (arg.contains("-v")) {
+                    System.out.println("Using Verbose Mode");
+                    verbose = true;
+                }                
+            }
+            
+            Socket socket = new Socket("localhost", 50000);
+            DataInputStream streamIn = new DataInputStream(socket.getInputStream());
+            DataOutputStream streamOut = new DataOutputStream(socket.getOutputStream());
+            
+            String stringBuffer = "";
+            while (!stringBuffer.equals("QUIT")) {
+                /// Writing to socket State
+                
+                /// Check current state and branch to code to respond to server response.
+
+                // if (stringBuffer.equals("HELO")) {
+                //     stringBuffer = System.getProperty("user.name");
+                // }
+
+                stringBuffer = "HELO";
+
+
+
+                streamOut.writeUTF(stringBuffer);
+                streamOut.flush();
+
+                if (verbose) {
+                    System.out.println(stringBuffer);
+                }
+                
+                /// Reading from socket State
+                stringBuffer = streamIn.readUTF(); 
+
+                if (verbose) {
+                    System.out.println(stringBuffer);
+                }
+            }
+        
+            streamIn.close();
+            streamOut.close();
+            socket.close();
+        }
     }
-
 }
