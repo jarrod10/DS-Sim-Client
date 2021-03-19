a2d6e1a14d342a17e80e3c890eb49946d2e64f20
Fred <frederick-lishia.jack@student.mq.edu.au>
2021-03-19 20:08:25 +1100

    Add XML parsing
    added first stage of parsing feature
    need to enter rest of the variables
    commented out for now

--- src/Protocol/ConcreteHandler/XMLHandler.java
+++ src/Protocol/ConcreteHandler/XMLHandler.java
@@ -10,8 +10,27 @@ public class XMLHandler implements Handler {
 
     @Override
     public Action enterState() {
-        // call XML handling functions
-
+//		try {
+//			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//			DocumentBuilder builder = factory.newDocumentBuilder();
+//			Document document = builder.parse(new File("ds-system.xml"));
+//			
+//		    XPathFactory xPathfactory = XPathFactory.newInstance();
+//		    XPath xpath = xPathfactory.newXPath();
+//		    XPathExpression expr = xpath.compile("//system/servers/server[@type]");
+//		    NodeList nl = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+//
+//		    for (int temp = 0; temp < nl.getLength(); temp++) {
+//		        Node currentItem = nl.item(temp);
+//		        String type = currentItem.getAttributes().getNamedItem("type").getNodeValue();
+//		        String limit = currentItem.getAttributes().getNamedItem("limit").getNodeValue();
+//		        System.out.println(type);
+//		    }
+//			
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
         return new Action();
     }
 
