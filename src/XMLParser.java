dadac2758ea059f4c89df432976ab98ba91106d3
Fred <frederick-lishia.jack@student.mq.edu.au>
2021-03-25 09:09:37 +1100

    updated XMLparser
    parsed String variables and added server info to array list

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -7,7 +7,7 @@ public class XMLParser {
 	public void parse() {
 		SystemInfomation info = SystemInfomation.getInstance();
 		//example of adding server to server collection class
-		info.addServerInfomation("Big", 3, 3, 1.0f, 1, 1, 1);
+		//info.addServerInfomation("Big", 3, 3, 1.0f, 1, 1, 1);
 
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -22,20 +22,20 @@ public class XMLParser {
 			for (int temp = 0; temp < nl.getLength(); temp++) {
 				Node currentItem = nl.item(temp);
 				String type = currentItem.getAttributes().getNamedItem("type").getNodeValue();
-				String limit = currentItem.getAttributes().getNamedItem("limit").getNodeValue();
-				String bootupTime = currentItem.getAttributes().getNamedItem("bootupTime").getNodeValue();
-				String hourlyRate = currentItem.getAttributes().getNamedItem("hourlyRate").getNodeValue();
-				String coreCount = currentItem.getAttributes().getNamedItem("coreCount").getNodeValue();
-				String memory = currentItem.getAttributes().getNamedItem("memory").getNodeValue();
-				String disk = currentItem.getAttributes().getNamedItem("disk").getNodeValue();
-				System.out.println(type);
-				System.out.println(limit);
-				System.out.println(bootupTime);
-				System.out.println(hourlyRate);
-				System.out.println(coreCount);
-				System.out.println(memory);
-				System.out.println(disk);
-
+				String _limit = currentItem.getAttributes().getNamedItem("limit").getNodeValue();
+				String _bootupTime = currentItem.getAttributes().getNamedItem("bootupTime").getNodeValue();
+				String _hourlyRate = currentItem.getAttributes().getNamedItem("hourlyRate").getNodeValue();
+				String _coreCount = currentItem.getAttributes().getNamedItem("coreCount").getNodeValue();
+				String _memory = currentItem.getAttributes().getNamedItem("memory").getNodeValue();
+				String _disk = currentItem.getAttributes().getNamedItem("disk").getNodeValue();
+				int limit=Integer.parseInt(_limit);
+				int bootupTime=Integer.parseInt(_bootupTime);
+				Float hourlyRate=Float.parseFloat(_hourlyRate);
+				int coreCount=Integer.parseInt(_coreCount);
+				int memory=Integer.parseInt(_memory);
+				int disk=Integer.parseInt(_disk);
+				info.addServerInfomation(type, limit, bootupTime, hourlyRate, coreCount, memory, disk);
+				
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
